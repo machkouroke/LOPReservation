6f45fb4f5e5b02f31ed46a01486eb1f6befde070
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-05-03 22:54:36 +0000

    Test Commit

--- src/com/lop/Main.java
+++ src/com/lop/Main.java
@@ -1,6 +1,6 @@
 package com.lop;
 
-import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatArcDarkIJTheme;
+// import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatArcDarkIJTheme;
 import com.lop.View.ConnectionWindow;
 
 import javax.swing.SwingUtilities;
@@ -9,11 +9,11 @@ import java.lang.reflect.InvocationTargetException;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, InvocationTargetException {
-        try {
-            UIManager.setLookAndFeel( new FlatArcDarkIJTheme() );
-        } catch( Exception ex ) {
-            System.err.println( "Failed to initialize LaF" );
-        }
+//         try {
+//             UIManager.setLookAndFeel( new FlatArcDarkIJTheme() );
+//         } catch( Exception ex ) {
+//             System.err.println( "Failed to initialize LaF" );
+//         }
         Runnable initFrame = ConnectionWindow::new;
         SwingUtilities.invokeAndWait(initFrame);
 
