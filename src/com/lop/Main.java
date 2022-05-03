e4123dfd0afbea5504df25bec418e4a95a000f0b
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-05-03 20:30:24 +0000

    Désactivation des thèmes

--- src/com/lop/Main.java
+++ src/com/lop/Main.java
@@ -1,19 +1,19 @@
 package com.lop;
 
-import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatArcDarkIJTheme;
-import com.lop.View.ConnectionWindow;
+//import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatArcDarkIJTheme;
+import com.lop.View.*;
 
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
+
 import java.lang.reflect.InvocationTargetException;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, InvocationTargetException {
-        try {
-            UIManager.setLookAndFeel( new FlatArcDarkIJTheme() );
-        } catch( Exception ex ) {
-            System.err.println( "Failed to initialize LaF" );
-        }
+//        try {
+//            UIManager.setLookAndFeel( new FlatArcDarkIJTheme() );
+//        } catch( Exception ex ) {
+//            System.err.println( "Failed to initialize LaF" );
+//        }
         Runnable initFrame = ConnectionWindow::new;
         SwingUtilities.invokeAndWait(initFrame);
 
