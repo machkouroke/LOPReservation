208c85dff2be4a359c60189167711a7f36911462
MorelK1 <kouhossounon.vianney@gmail.com>
2022-05-04 18:20:53 +0000

    Commentaire dur des lignes

--- src/main/java/com/lop/Main.java
+++ src/main/java/com/lop/Main.java
@@ -11,11 +11,11 @@ import java.lang.reflect.InvocationTargetException;
 public class Main {
     public static void main(String[] args) throws InterruptedException, InvocationTargetException {
 
-         try {
-             UIManager.setLookAndFeel( new FlatArcDarkIJTheme() );
-         } catch( Exception ex ) {
-             System.err.println( "Failed to initialize LaF" );
-         }
+        //  try {
+        //      UIManager.setLookAndFeel( new FlatArcDarkIJTheme() );
+        //  } catch( Exception ex ) {
+        //      System.err.println( "Failed to initialize LaF" );
+        //  }
         Runnable initFrame = ConnectionWindow::new;
         SwingUtilities.invokeAndWait(initFrame);
 
