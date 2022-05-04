3986c85fcb76c3950c0db497288b50fa7931f7e1
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-05-04 02:59:03 +0000

    Ajout de la compatiblité Maven et des dépendances flatlaf

--- src/main/java/com/lop/Main.java
+++ src/main/java/com/lop/Main.java
@@ -1,23 +1,21 @@
 package com.lop;
 
 
-//import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatArcDarkIJTheme;
+import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatArcDarkIJTheme;
 import com.lop.View.ConnectionWindow;
 
-
-
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
 import java.lang.reflect.InvocationTargetException;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, InvocationTargetException {
 
-//         try {
-//             UIManager.setLookAndFeel( new FlatArcDarkIJTheme() );
-//         } catch( Exception ex ) {
-//             System.err.println( "Failed to initialize LaF" );
-//         }
+         try {
+             UIManager.setLookAndFeel( new FlatArcDarkIJTheme() );
+         } catch( Exception ex ) {
+             System.err.println( "Failed to initialize LaF" );
+         }
         Runnable initFrame = ConnectionWindow::new;
         SwingUtilities.invokeAndWait(initFrame);
 
