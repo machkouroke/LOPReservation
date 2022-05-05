a8125dab604375aae6b3a8ea5cc9294c6535ff82
machkouroke <40785379+machkouroke@users.noreply.github.com>
2022-05-05 16:34:59 +0000

    Add try with ressource to some function

--- src/main/java/com/lop/model/dao/PrintEvent.java
+++ src/main/java/com/lop/model/dao/PrintEvent.java
@@ -8,37 +8,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-public class PrintEvent {
-    private final Factory factory;
-
-    public PrintEvent(Factory factory) {
-        this.factory = factory;
-    }
+public record PrintEvent(Factory factory) {
 
     public List<List<String>> print(String query) throws SQLException {
-        Statement statement = this.factory.getConnection().createStatement();
-        ResultSet answer = statement.executeQuery(query);
-        List<List<String>> data = new ArrayList<>();
+        try(Statement statement = this.factory.getConnection().createStatement()) {
+            ResultSet answer = statement.executeQuery(query);
+            List<List<String>> data = new ArrayList<>();
 
-        /*I use this ResultSet to have some information like ColumnName and ColumnCount*/
-        ResultSetMetaData metadata = answer.getMetaData();
-        int numberOfColumn = metadata.getColumnCount();
-        List<String> header = new ArrayList<>();
+            /*I use this ResultSet to have some information like ColumnName and ColumnCount*/
+            ResultSetMetaData metadata = answer.getMetaData();
+            int numberOfColumn = metadata.getColumnCount();
+            List<String> header = new ArrayList<>();
 
-        /*Header add*/
-        for (int i = 1; i < numberOfColumn + 1; i++) {
-            header.add(metadata.getColumnName(i));
-        }
-        data.add(header);
-
-        /*Row add*/
-        while (answer.next()) {
-            List<String> row = new ArrayList<>();
+            /*Header add*/
             for (int i = 1; i < numberOfColumn + 1; i++) {
-                row.add( answer.getNString(i));
+                header.add(metadata.getColumnName(i));
             }
-            data.add(row);
+            data.add(header);
+
+            /*Row add*/
+            while (answer.next()) {
+                List<String> row = new ArrayList<>();
+                for (int i = 1; i < numberOfColumn + 1; i++) {
+                    row.add(answer.getNString(i));
+                }
+                data.add(row);
+            }
+            return data;
         }
-        return data;
+
     }
 }
